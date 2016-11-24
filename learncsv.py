0d64ad400442ed172fdc22ced63c3f2ed770e2d9
Deaglan Crew <d.crew@liv.ac.uk>
2016-11-24 02:26:10 +0000

    Added initial scripts.

---
+++ learncsv.py
@@ -0,0 +1,11 @@
+import csv
+
+def read_valid_universities (file, removeEmpty):
+    """
+    This function will read the contents of the csv file 'uniData.csv' using
+    the csv.DictReader generator, it will return a list of entries, if
+    if removeEmpty is set to True, it will skip entries with no data on part
+    time students.
+    """
+    return
+
---
+++ testcsv.py
@@ -0,0 +1,16 @@
+import unittest
+import learncsv
+
+class CsvTestCase(unittest.TestCase):
+    def setUp(self):
+        self.withBlock = [i["Provider"] for i in
+                          learncsv.read_valid_universities("uniData.csv", True)]
+        self.sansBlock = [i["Provider"] for i in
+                          learncsv.read_valid_universities("uniData.csv", False)]
+
+    def test_block(self):
+        self.assertNotIn("The Institute of Cancer Research", self.withBlock)
+        self.assertIn("The Institute of Cancer Research", self.sansBlock)
+
+unittest.main()
+
